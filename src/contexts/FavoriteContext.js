7637f8c88266c8567ea5fdabcff7fb3c81e974b0
dev-tolga <79405453+dev-tolga@users.noreply.github.com>
2022-03-06 12:29:25 +0300

    cp

--- src/contexts/FavoriteContext.js
+++ src/contexts/FavoriteContext.js
@@ -1,43 +1,46 @@
 import React, { useState, useContext, useEffect } from "react";
 import { blogs } from "../../assets/data/blogs";
-import {getData,getAllKeys,storeData,removeData,clearAll } from "../library/helpers/asyncStorage";
+import {
+  getData,
+  getAllKeys,
+  storeData,
+  removeData,
+  clearAll,
+} from "../library/helpers/asyncStorage";
 
 const FavoriteContext = React.createContext();
 
 export const FavoriteProvider = ({ children }) => {
-    
-   const [favoritedBlogList, setFavoritedBlogList] = useState([]);
+  const [favoritedBlogList, setFavoritedBlogList] = useState([]);
 
-   useEffect(() => {
-     const getFavoritedBlogs = async () => {
-       const _favoritedBlogList = await getData("favoritedBlogs");
-       _favoritedBlogList && setFavoritedBlogList(_favoritedBlogList);
-     };
-     getFavoritedBlogs();
-   }, []);
+  useEffect(() => {
+    const getFavoritedBlogs = async () => {
+      const _favoritedBlogList = await getData("favoritedBlogs");
+      _favoritedBlogList && setFavoritedBlogList(_favoritedBlogList);
+    };
+    getFavoritedBlogs();
+  }, []);
 
-   const handleFavorite = (blogId) => {
-       console.log(id);
-     if (favoritedBlogList.includes(blogId)) {
-       const _favoritedBlogList = favoritedBlogList.filter(
-         (id) => id !== blogId
-       );
-       setFavoritedBlogList(_favoritedBlogList);
-       storeData("favoritedBlogs", _favoritedBlogList);
-     } else {
-       const _favoritedBlogList = [...favoritedBlogList, blogId];
-       setFavoritedBlogList(_favoritedBlogList);
-       storeData("favoritedBlogs", _favoritedBlogList);
-     }
-   };
+  const handleFavorite = (blogId) => {
+      
+    if (favoritedBlogList.includes(blogId)) {
+      const _favoritedBlogList = favoritedBlogList.filter(
+        (id) => id !== blogId
+      );
+      setFavoritedBlogList(_favoritedBlogList);
+      storeData("favoritedBlogs", _favoritedBlogList);
+    } else {
+      const _favoritedBlogList = [...favoritedBlogList, blogId];
+      setFavoritedBlogList(_favoritedBlogList);
+      storeData("favoritedBlogs", _favoritedBlogList);
+    }
+  };
 
-  
-
- return (
-   <FavoriteContext.Provider value={{ favoritedBlogList, handleFavorite }}>
-     {children}
-   </FavoriteContext.Provider>
- );
+  return (
+    <FavoriteContext.Provider value={{ favoritedBlogList, handleFavorite }}>
+      {children}
+    </FavoriteContext.Provider>
+  );
 };
 export const useFavoriteContext = () => {
   return useContext(FavoriteContext);
